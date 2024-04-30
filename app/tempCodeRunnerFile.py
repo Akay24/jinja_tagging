1a4dbe521867bea585f2411abdfdc6745d4cc433
ABHIJEET MISHRA <116637451+Akay24@users.noreply.github.com>
2024-04-30 08:47:03 +0530

    jinja_tagging

---
+++ app/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+
+    # pho = 0987654456789
+    # email = 'akaayroot656@gmail.com'
+    # add ='bhubaneswar'
+    # ED = Emp(ename = name , pho = pho,email = email,add = add)
+    # return HttpResponse("employee registered succesfully")
